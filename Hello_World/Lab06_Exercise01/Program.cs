3851e5c5aa207fb3c6aa3a2791da4802872ed595
suwannarat30 <suaqeiq@gmail.com>
2025-01-10 19:58:05 +0700

    ส่งงาน

---
+++ Hello_World/Lab06_Exercise01/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+System.Console.WriteLine("Hello, I'm 66030196");
+System.Console.WriteLine("Enter your name");
+var name = System.Console.ReadLine();  
+System.Console.WriteLine($"Your name is {name}");
